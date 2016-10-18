7005f7df54656a508ef5f7b40a17caa22ff679ec
Jon Friis <friis.j@gmail.com>
2016-10-18 18:28:37 -0400

    Product page layout basics

---
+++ _site/js/layout.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+	// Button
+
+	// Quantity
+	$('#qty__input').focus();
+	$('#qty__spinner--up').click(function(){
+        var $qty=$('#qty__input');
+        var currentVal = parseInt($qty.val());
+        if (!isNaN(currentVal)) {
+            $qty.val(currentVal + 1);
+        }
+    });
+    $('#qty__spinner--down').click(function(){
+        var $qty=$('#qty__input');
+        var currentVal = parseInt($qty.val());
+        if (!isNaN(currentVal) && currentVal > 0) {
+            $qty.val(currentVal - 1);
+        }
+    });
+
+
+});
