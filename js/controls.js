4f2ee2108a66b2bbde6e5ece1d7b852c724a60a2
Jon Friis <friis.j@gmail.com>
2016-12-27 15:53:23 -0500

    Layout adjustments

--- js/controls.js
+++ js/controls.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
 
     // Quantity
 	//$('#qty__input').focus();
-	$('#qty__spinner--up').click(function(){
-        var $qty=$('#qty__input');
+	$('.qty__spinner--up').click(function(){
+        var $qty=$('.qty__input');
         var currentVal = parseInt($qty.val());
         if (!isNaN(currentVal)) {
             $qty.val(currentVal + 1);
         }
     });
-    $('#qty__spinner--down').click(function(){
-        var $qty=$('#qty__input');
+    $('.qty__spinner--down').click(function(){
+        var $qty=$('.qty__input');
         var currentVal = parseInt($qty.val());
         if (!isNaN(currentVal) && currentVal > 0) {
             $qty.val(currentVal - 1);
